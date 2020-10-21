c1bf9604e597cbce233bc586fde0d90b3e28bc65
sam632 <60163384+sam632@users.noreply.github.com>
2020-10-21 18:57:41 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,205 @@
+/*
+ * Example of using esp-homekit library to control
+ * a Sonoff S26 using HomeKit.
+ * The esp-wifi-config library is also used in this
+ * example. This means you don't have to specify
+ * your network's SSID and password before building.
+ *
+ * In order to flash the sonoff S26 you will have to
+ * have a 3,3v (logic level) FTDI adapter.
+ *
+ * To flash this example solder wires to 3,3v, TX, RX, GND
+ * pins and connect to FTDI.
+ * Next hold down the button and connect the FTDI adapter
+ * to your computer. The sonoff is now in flash mode and
+ * you can flash the custom firmware.
+ *
+ * WARNING: Do not connect the sonoff to AC while it's
+ * connected to the FTDI adapter! This may fry your
+ * computer and sonoff.
+ *
+ */
+
+#include <stdio.h>
+#include <espressif/esp_wifi.h>
+#include <espressif/esp_sta.h>
+#include <espressif/esp_common.h>
+#include <esp/uart.h>
+#include <esp8266.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include <wifi_config.h>
+
+#include <button.h>
+
+// The GPIO pin that is connected to the relay on the Sonoff Basic.
+const int relay_gpio = 12;
+// The GPIO pin that is connected to the LED on the Sonoff Basic.
+const int led_gpio = 13;
+// The GPIO pin that is connected to the button on the Sonoff Basic.
+const int button_gpio = 0;
+
+void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
+void button_callback(button_event_t event, void* context);
+
+void relay_write(bool on) {
+    gpio_write(relay_gpio, on ? 1 : 0);
+}
+
+void led_write(bool on) {
+    gpio_write(led_gpio, on ? 0 : 1);
+}
+
+void reset_configuration_task() {
+    //Flash the LED first before we start the reset
+    for (int i=0; i<3; i++) {
+        led_write(true);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        led_write(false);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+    
+    printf("Resetting Wifi Config\n");
+    wifi_config_reset();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    printf("Resetting HomeKit Config\n");
+    homekit_server_reset();
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    printf("Restarting\n");
+    sdk_system_restart();
+    vTaskDelete(NULL);
+}
+
+void reset_configuration() {
+    printf("Resetting Sonoff configuration\n");
+    xTaskCreate(reset_configuration_task, "Reset configuration", 256, NULL, 2, NULL);
+}
+
+homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(
+    ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback)
+);
+
+void gpio_init() {
+    gpio_enable(led_gpio, GPIO_OUTPUT);
+    led_write(switch_on.value.bool_value);
+    gpio_enable(relay_gpio, GPIO_OUTPUT);
+    relay_write(switch_on.value.bool_value);
+}
+
+void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
+    relay_write(switch_on.value.bool_value);
+    led_write(switch_on.value.bool_value);
+}
+
+void button_callback(button_event_t event, void* context) {
+    switch (event) {
+        case button_event_single_press:
+            printf("Toggling relay\n");
+            switch_on.value.bool_value = !switch_on.value.bool_value;
+            relay_write(switch_on.value.bool_value);
+            led_write(switch_on.value.bool_value);
+            homekit_characteristic_notify(&switch_on, switch_on.value);
+            break;
+        case button_event_long_press:
+            reset_configuration();
+            break;
+        default:
+            printf("Unknown button event: %d\n", event);
+    }
+}
+
+void button_init() {
+    button_config_t button_config = BUTTON_CONFIG(
+        button_active_low,
+        .long_press_time = 3000,
+        .max_repeat_presses = 1,
+    );
+
+    if (button_create(button_gpio, button_config, button_callback, NULL)) {
+        printf("Failed to initialize button\n");
+    }
+}
+
+void outlet_identify_task(void *_args) {
+    // We identify the Sonoff by Flashing it's LED.
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            led_write(true);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            led_write(false);
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+        }
+
+        vTaskDelay(250 / portTICK_PERIOD_MS);
+    }
+
+    led_write(false);
+
+    vTaskDelete(NULL);
+}
+
+void outlet_identify(homekit_value_t _value) {
+    printf("Outlet Identify\n");
+    xTaskCreate(outlet_identify_task, "Outlet Identify", 128, NULL, 2, NULL);
+}
+
+homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, "Socket");
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_outlet, .services=(homekit_service_t*[]){
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]){
+            &name,
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Sam"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "SLMPSWP1"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "Socket"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.0.1"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, outlet_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(OUTLET, .primary=true, .characteristics=(homekit_characteristic_t*[]){
+            HOMEKIT_CHARACTERISTIC(NAME, "Socket"),
+            &switch_on,
+	    HOMEKIT_CHARACTERISTIC(OUTLET_IN_USE, true),
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "933-67-390",
+    .setupId="4AS3",
+};
+
+void on_wifi_ready() {
+    homekit_server_init(&config);
+}
+
+void create_accessory_name() {
+    uint8_t macaddr[6];
+    sdk_wifi_get_macaddr(STATION_IF, macaddr);
+    
+    int name_len = snprintf(NULL, 0, "Socket-%02X%02X%02X",
+                            macaddr[3], macaddr[4], macaddr[5]);
+    char *name_value = malloc(name_len+1);
+    snprintf(name_value, name_len+1, "Socket-%02X%02X%02X",
+             macaddr[3], macaddr[4], macaddr[5]);
+    
+    name.value = HOMEKIT_STRING(name_value);
+}
+
+void user_init(void) {
+    uart_set_baud(0, 115200);
+
+    create_accessory_name();
+    
+    wifi_config_init("Socket", NULL, on_wifi_ready);
+    gpio_init();
+    button_init();
+
+}
